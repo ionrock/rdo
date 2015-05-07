dbda186235efc4dfabdd4b489b42e36847588263
Eric Larson <eric.larson@rackspace.com>
2015-05-06 19:20:12 -0500

    Bump version: 0.1.0 → 0.1.1

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ requirements = []
 
 setup(
     name='rdo',
-    version='0.1.0',
+    version='0.1.1',
     description='RemoteDO command',
     long_description=readme,
     author='Eric Larson',
