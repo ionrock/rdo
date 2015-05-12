41a0fa6412427dadfb33c77da45bc88c576fa67c
Eric Larson <eric.larson@rackspace.com>
2015-05-12 17:26:46 -0500

    Add a common function for deriving the working dir.

--- rdo/drivers/base.py
+++ rdo/drivers/base.py
@@ -6,6 +6,13 @@ class BaseDriver(object):
     def __init__(self, config):
         self.config = config
 
+    def working_dir(self, cmd):
+        command = ' '.join(cmd)
+        working_dir = self.config.get('directory')
+        if working_dir:
+            command = 'cd %s && %s' % (working_dir, command)
+        return command
+
     def do(self, cmd):
         cmd = self.command(cmd)
         call(cmd)
