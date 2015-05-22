0e5db5420b941574f7074e716159d85b3097ae58
Eric Larson <eric.larson@rackspace.com>
2015-05-22 13:03:51 -0500

    use vagrant as the default.

--- rdo/config.py
+++ rdo/config.py
@@ -3,12 +3,21 @@ import functools
 
 from ConfigParser import ConfigParser
 
+# Use vagrant by default
+DEFAULTS = {
+    'driver': 'vagrant',
+    'directory': '/vagrant'
+}
+
 
 def get_config(config_file='.rdo.conf'):
     config = ConfigParser()
     config.read(config_file)
     env = os.environ.get('RDO_ENV') or 'default'
-    return dict(config.items(env))
+    try:
+        return dict(config.items(env))
+    except ConfigParser.NoSectionError:
+        return DEFAULTS
 
 
 if __name__ == '__main__':
