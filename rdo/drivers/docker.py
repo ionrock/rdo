8b85f062d74ba0a069f4b486983cf25bb77fd43b
Eric Larson <eric.larson@rackspace.com>
2015-09-03 00:39:20 -0500

    Use a container rather than an image
    The idea here is somewhat opinionated.
    
     1. start a container from an image and leave it running bash as a
        daemon
     2. grab the container id and store it in .rdo.docker.container_id
     3. use the container id with docker exec to run commands in the
        container persisting them to the running image
     4. commit to the same image after each command
    
    So, this should replace the current image. I'm not sure if you hit the
    limits of the docker revisions (128?) or not. I bet re-using the name is
    a bad idea if someone just uses "ubuntu" for example.
    
    Bad unintended consequences aside, using the container vs. an image
    seems much better and gets you something closer to how you use
    vagrant. The advantage here is that you can dev somewhat normally
    whereas if you use images, you end can up with a pretty ridiculus
    Dockerfile or script that gets run when the docker image gets built.

--- rdo/drivers/docker.py
+++ rdo/drivers/docker.py
@@ -1,12 +1,90 @@
+"""
+The docker driver current assumes you'll use the following workflow:
+
+ 1. Start a container from some image
+ 2. Run commands in that container (using docker exec)
+
+This will allow running commands that change the filesystem and have
+them persist.
+"""
+import os
+
+from subprocess import check_output, call
 from rdo.drivers.base import BaseDriver
+
+
+class Container(object):
+    id_fname = '.rdo.docker.container_id'
+
+    def __init__(self, image, directory):
+        self.image = image
+        self.directory = directory
+        self._id = ''
+
+    @property
+    def id(self):
+        if not self._id:
+            try:
+                self._id = open(self.id_fname).read().strip()
+            except IOError:
+                pass
+        return self._id
+
+    def is_running(self):
+        for cid in check_output(['docker', 'ps', '-q']).split('\n'):
+            if self.id and self.id.startswith(cid):
+                return True
+        return False
+
+    def start_container(self):
+        cmd = [
+            'docker', 'run', '-it', '-d',
+            '-v', '%s:%s' % (os.getcwd(), self.directory),
+            self.image, 'bash'
+        ]
+        print('Starting conainer: %s' % (' '.join(cmd)))
+        return check_output(cmd).strip()
+
+    def start(self):
+        """Idempotent function to start the container"""
+        if not self.is_running():
+            cid = self.start_container()
+            with open(self.id_fname, 'w+') as fh:
+                fh.write(cid)
+        return self.id
+
+
+class PersistentContainer(Container):
+
+    def commit(self):
+        cmd = ['docker', 'commit', self.id, self.image]
+        call(cmd)
+
+    def __enter__(self):
+        self.start()
+        return self
+
+    def __exit__(self, type, value, traceback):
+        if not traceback:
+            self.commit()
+            return True
+        return False
 
 
 class DockerDriver(BaseDriver):
 
     def command(self, cmd):
-        action = 'run'
-        if self.config.get('exec'):
-            action = 'exec'
+        # The command gets prefixed in the do method
+        return cmd
 
-        prefix = ['docker', action, '-it', self.config.get('name')]
         return prefix + cmd
+
+    def do(self, cmd):
+        prefix = ['docker', 'exec']
+        container = PersistentContainer(
+            self.config.get('name'),
+            self.config.get('directory')
+        )
+        with container as c:
+            prefix.append(c.id)
+            super(DockerDriver, self).do(prefix + cmd)
