1c58abdf67d69e832cadc8088b62093d2b8ca75f
Eric Larson <eric@ionrock.org>
2016-02-19 10:59:13 -0600

    Refactor the Vagrant driver

--- rdo/drivers/vagrant.py
+++ rdo/drivers/vagrant.py
@@ -4,8 +4,9 @@ from rdo.drivers.base import BaseDriver
 class VagrantDriver(BaseDriver):
 
     def command(self, cmd):
-        prefix = 'vagrant ssh -c'.split()
-        cmd = ' '.join(cmd)
         cmd = self.use_sudo(cmd)
         cmd = self.working_dir(cmd)
-        return prefix + [cmd]
+
+        # We need the command to be a single arg when passing it to
+        # vagrant ssh -c
+        return ['vagrant', 'ssh', '-c', ' '.join(cmd)]
